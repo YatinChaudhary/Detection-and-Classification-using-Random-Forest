08d6790bff11230dd579148a631129f3a9248bb8
Yatin Chaudhary <yatin.chaudhary@tum.de>
2018-01-07 01:33:09 +0100

    updated function for creating training data for a DTree

--- Random_Forest/Object_classification.cpp
+++ Random_Forest/Object_classification.cpp
@@ -3,33 +3,62 @@
 #include "opencv2\ml.hpp"
 #include "hog_visualization.h"
 
-int get_data_and_labels(cv::String path, cv::Mat & features, cv::Mat & labels);
+int get_data_and_labels(cv::String path, cv::Mat & features, cv::Mat & labels, cv::Size & roi_size);
 
 int main()
 {
 	// creating DTree
-	//std::cout << "\nchk-1" << std::endl;
 	cv::Ptr<cv::ml::DTrees> dtree = cv::ml::DTrees::create();
 	cv::Mat features;
 	cv::Mat labels;
-	//std::cout << "\nchk-2" << std::endl;
-	//cv::waitKey(5000);
-	int x = get_data_and_labels("D:\\M.Sc\\Semester_3\\Tracking and Detection in Computer Vision\\Exercises\\2\\data\\task2\\train", features, labels);
-	//cv::waitKey(5000);
+	cv::Size roi(50, 50);
+
+	int x = get_data_and_labels("D:\\M.Sc\\Semester_3\\Tracking and Detection in Computer Vision\\Exercises\\2\\data\\task2\\train", features, labels, roi);
+	if (x == 0)
+	{
+		std::cout << "\n\nFeature vector size:   " << features.size << std::endl;
+		std::cout << "\n\nLabel vector size:   " << labels.size << std::endl;
+	}
 	return 0;
 }
 
-int get_data_and_labels(cv::String path, cv::Mat & features, cv::Mat & labels)
+int get_data_and_labels(cv::String path, cv::Mat & features, cv::Mat & labels, cv::Size & roi_size)
 {
 	DIR *dir, *subdir;
 	struct dirent *ent, *ent_subdir;
+	cv::Mat image, crop(roi_size, CV_32F), row;
+	cv::Rect roi;
+	std::string subdir_path, image_path;
+
 	if ((dir = opendir(path.c_str())) != NULL) {
-		/* print all the files and directories within directory */
 		while ((ent = readdir(dir)) != NULL) {
-			printf("%s\n", ent->d_name);
-			if ((subdir = opendir(std::string(path + "\\" + ent->d_name).c_str())) != NULL) {
-				while ((ent_subdir = readdir(subdir)) != NULL) {
-					printf("%s\n", ent_subdir->d_name);
+			//printf("%s\n", ent->d_name);
+			if ((std::string(ent->d_name) != std::string(".")) &&
+				(std::string(ent->d_name) != std::string("..")))
+			{
+				subdir_path = path + "\\" + ent->d_name;
+				if ((subdir = opendir(subdir_path.c_str())) != NULL) {
+					while ((ent_subdir = readdir(subdir)) != NULL) {
+						//printf("%s\n", ent_subdir->d_name);
+						if ((std::string(ent_subdir->d_name) != std::string(".")) &&
+							(std::string(ent_subdir->d_name) != std::string("..")))
+						{
+							image_path = subdir_path + "\\" + ent_subdir->d_name;
+							image = cv::imread(image_path, 1);
+							roi = cv::Rect(image.cols/2 - roi_size.width/2,
+										   image.rows/2 - roi_size.height/2,
+										   roi_size.width, roi_size.height);
+							image(roi).copyTo(crop);
+							// row includes the 3 channels of the original image
+							// do we need all the channels?
+							row = crop.reshape(1, 1);
+							row.convertTo(row, CV_32F);
+							features.push_back(row);
+							labels.push_back(std::stoi(ent->d_name));
+						}
+					}
+
+					closedir(subdir);
 				}
 			}
 		}
